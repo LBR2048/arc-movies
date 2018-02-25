3a18d22a435bf79dcf7f18db5c9cfd3e242c186e
LBR2048 <leo.ardjomand@gmail.com>
2018-02-25 17:04:03 -0300

    Add data models for upcoming movies and movie details

---
+++ app/src/main/java/ardjomand/leonardo/arcmovies/model/UpcomingMovies.java
@@ -0,0 +1,66 @@
+package ardjomand.leonardo.arcmovies.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by unity on 25/02/18.
+ */
+
+public class UpcomingMovies {
+
+    @SerializedName("results")
+    @Expose
+    private List<Movie> results = null;
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    @Override
+    public String toString() {
+        return "UpcomingMovies{" +
+                "results=" + results +
+                ", page=" + page +
+                '}';
+    }
+}
