f635f6ed6c3c0b77a78158efaf68337b998eac9e
LBR2048 <leo.ardjomand@gmail.com>
2018-02-25 17:48:41 -0300

    Add upcoming movies presenter

---
+++ app/src/main/java/ardjomand/leonardo/arcmovies/movies/UpcomingMoviesPresenter.java
@@ -0,0 +1,44 @@
+package ardjomand.leonardo.arcmovies.movies;
+
+import java.util.List;
+
+import ardjomand.leonardo.arcmovies.data.MoviesRepository;
+import ardjomand.leonardo.arcmovies.model.Movie;
+
+/**
+ * Created by unity on 25/02/18.
+ */
+
+class UpcomingMoviesPresenter implements UpcomingMoviesContract.Presenter {
+
+    private final UpcomingMoviesContract.View mView;
+    private final MoviesRepository mRepository;
+
+    public UpcomingMoviesPresenter(UpcomingMoviesContract.View view, MoviesRepository moviesRepository) {
+        mView = view;
+        mRepository = moviesRepository;
+    }
+
+    @Override
+    public void loadUpcomingMovies() {
+        mView.setLoading(true);
+        mRepository.loadUpcomingMovies(new MoviesRepository.LoadUpcomingMoviesCallback() {
+            @Override
+            public void onSuccess(List<Movie> movies) {
+                mView.setLoading(false);
+                mView.showUpcomingMovies(movies);
+            }
+
+            @Override
+            public void onFailure() {
+                mView.setLoading(false);
+                mView.showErrorMessage();
+            }
+        });
+    }
+
+    @Override
+    public void onMovieClicked(int movieId) {
+        mView.openMovie(movieId);
+    }
+}
