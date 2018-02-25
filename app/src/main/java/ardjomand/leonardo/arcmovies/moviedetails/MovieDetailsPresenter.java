d88c63b11e5ea7b2cfbeb87daa9be45c9852ed1e
LBR2048 <leo.ardjomand@gmail.com>
2018-02-25 17:48:50 -0300

    Add movie details presenter

---
+++ app/src/main/java/ardjomand/leonardo/arcmovies/moviedetails/MovieDetailsPresenter.java
@@ -0,0 +1,37 @@
+package ardjomand.leonardo.arcmovies.moviedetails;
+
+import ardjomand.leonardo.arcmovies.data.MoviesRepository;
+import ardjomand.leonardo.arcmovies.model.Movie;
+
+/**
+ * Created by unity on 25/02/18.
+ */
+
+class MovieDetailsPresenter implements MovieDetailsContract.Presenter {
+
+    private final MovieDetailsContract.View mView;
+    private final MoviesRepository mRepository;
+
+    public MovieDetailsPresenter(MovieDetailsContract.View view, MoviesRepository moviesRepository) {
+        mView = view;
+        mRepository = moviesRepository;
+    }
+
+    @Override
+    public void loadMovieDetails(int movieId) {
+        mView.setLoading(true);
+        mRepository.loadMovieDetails(new MoviesRepository.LoadMovieDetailsCallback() {
+            @Override
+            public void onSuccess(Movie movieDetails) {
+                mView.setLoading(false);
+                mView.showMovieDetails(movieDetails);
+            }
+
+            @Override
+            public void onFailure() {
+                mView.setLoading(false);
+                mView.showErrorMessage();
+            }
+        }, movieId);
+    }
+}
