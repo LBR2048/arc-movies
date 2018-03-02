cc5b22868800c2877384aa1f6e12dc97dd4b2284
Leonardo Ardjomand <leonardo.ardjomand@fluig.com>
2018-03-02 18:05:04 -0300

    Show posters on upcoming movies list

--- app/src/main/java/ardjomand/leonardo/arcmovies/data/remote/MoviesRemoteRepository.java
+++ app/src/main/java/ardjomand/leonardo/arcmovies/data/remote/MoviesRemoteRepository.java
@@ -26,6 +26,7 @@ public class MoviesRemoteRepository implements MoviesRepository{
 
     private static final String LOG_TAG = MoviesRemoteRepository.class.getSimpleName();
     private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static final String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w500";
     private static final String QUERY_PARAMETER_API_KEY = "api_key";
     private static final String API_KEY = "1f54bd990f1cdfb230adb312546d765d";
 
