2210b913774716f2db4de92ceebf28bb243252af
LBR2048 <leo.ardjomand@gmail.com>
2018-03-04 12:04:52 -0300

    Make UpcomingMovie implement Parcelable

--- app/src/main/java/ardjomand/leonardo/arcmovies/model/UpcomingMovie.java
+++ app/src/main/java/ardjomand/leonardo/arcmovies/model/UpcomingMovie.java
@@ -1,15 +1,19 @@
 package ardjomand.leonardo.arcmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by unity on 25/02/18.
  */
 
-public class UpcomingMovie {
+public class UpcomingMovie implements Parcelable {
 
     @SerializedName("vote_count")
     @Expose
@@ -173,4 +177,60 @@ public class UpcomingMovie {
                 ", title='" + title + '\'' +
                 '}';
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.voteCount);
+        dest.writeValue(this.id);
+        dest.writeValue(this.video);
+        dest.writeValue(this.voteAverage);
+        dest.writeString(this.title);
+        dest.writeValue(this.popularity);
+        dest.writeString(this.posterPath);
+        dest.writeString(this.originalLanguage);
+        dest.writeString(this.originalTitle);
+        dest.writeList(this.genreIds);
+        dest.writeString(this.backdropPath);
+        dest.writeValue(this.adult);
+        dest.writeString(this.overview);
+        dest.writeString(this.releaseDate);
+    }
+
+    public UpcomingMovie() {
+    }
+
+    protected UpcomingMovie(Parcel in) {
+        this.voteCount = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.video = (Boolean) in.readValue(Boolean.class.getClassLoader());
+        this.voteAverage = (Double) in.readValue(Double.class.getClassLoader());
+        this.title = in.readString();
+        this.popularity = (Double) in.readValue(Double.class.getClassLoader());
+        this.posterPath = in.readString();
+        this.originalLanguage = in.readString();
+        this.originalTitle = in.readString();
+        this.genreIds = new ArrayList<Integer>();
+        in.readList(this.genreIds, Integer.class.getClassLoader());
+        this.backdropPath = in.readString();
+        this.adult = (Boolean) in.readValue(Boolean.class.getClassLoader());
+        this.overview = in.readString();
+        this.releaseDate = in.readString();
+    }
+
+    public static final Parcelable.Creator<UpcomingMovie> CREATOR = new Parcelable.Creator<UpcomingMovie>() {
+        @Override
+        public UpcomingMovie createFromParcel(Parcel source) {
+            return new UpcomingMovie(source);
+        }
+
+        @Override
+        public UpcomingMovie[] newArray(int size) {
+            return new UpcomingMovie[size];
+        }
+    };
 }
